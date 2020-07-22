7abecdfbb943023eeef7ab20b95dc971012b38f5
saaaaraaaah <stre757@aucklanduni.ac.nz>
2020-07-23 02:59:59 +1200

    first commit

---
+++ StudentSIMS/StudentSIMS/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentSIMS.Models
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNumber { get; set; }
+        [Timestamp]
+        public DateTime TimeCreated { get; set; }
+    }
+}
