d3ab0f841594c1883325f0167311a7551fd52378
saaaaraaaah <stre757@aucklanduni.ac.nz>
2020-07-23 03:11:35 +1200

    created Address model class

---
+++ StudentSIMS/StudentSIMS/Models/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSIMS.Models
+{
+    public class Address
+    {
+        [Key]
+        public int StudentID { get; set; }
+        public int StreetNumber { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string Country { get; set; }
+
+    }
+}
